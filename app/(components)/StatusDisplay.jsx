b5870a0e166616d94879d4f815c311667875f6c0
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-05-08 14:26:03 +0100

    created dynamic route to deletee tickets

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,7 +1,7 @@
-const StatusDisplay = () => {
+const StatusDisplay = ({status}) => {
   return (
     <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold bg-green-300">
-        done
+        {status}
     </span>
   )
 }
